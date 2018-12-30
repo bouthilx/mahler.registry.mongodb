1f8a271e7b4d923ee38de0a82049f1197d64311a
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-12-29 22:15:14 -0500

    Add pymongo dependency

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup_args = dict(
             'mongodb = mahler.registry.mongodb'
             ],
         },
-    install_requires=['mahler.core'],
+    install_requires=['mahler.core', 'pymongo'],
     tests_require=tests_require,
     setup_requires=['setuptools', 'pytest-runner>=2.0,<3dev'],
     extras_require=dict(test=tests_require),
